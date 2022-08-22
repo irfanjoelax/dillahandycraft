8fd605d3b229f64abeab01dd240b556ef4551cbd
irfanjoelax <irfanthejoelax.com>
2022-08-22 17:25:47 +0800

    update form search layout app

--- resources/views/layouts/_form-search.blade.php
+++ resources/views/layouts/_form-search.blade.php
@@ -1,5 +1,4 @@
-<form class="input-group" method="POST" action="">
-    @csrf
+<form class="input-group" method="GET" action="">
     <input type="search" name="keyword" size="40" class="form-control" placeholder="Search Product...">
     <button class="btn btn-outline-primary" type="submit">
         <i class="fa-solid fa-magnifying-glass"></i>
